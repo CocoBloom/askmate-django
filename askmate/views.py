6bb872e7ee952d6855750ec149d8c4b0431ca99a
CocoBloom <virag.baranyai2@gmail.com>
2021-04-02 22:56:43 +0200

    change render()

--- askmate/views.py
+++ askmate/views.py
@@ -3,13 +3,32 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Virág',
+        'title': 'First Question',
+        'content': 'This is my first question  in Django',
+        'date_posted': 'Apr 2, 2020'
+    },
+    {
+        'author': 'Coco',
+        'title': 'First Coco',
+        'content': 'This is my Coco in Django',
+        'date_posted': 'Apr 1, 2020'
+    }
+]
+
 
 def home(request):
-    return HttpResponse("This is the Home page")
+    context = {
+        'posts': posts,
+        'title': 'Juhuu page'
+    }
+    return render(request, 'askmate/home.html', context)
 
 
 def about(request):
-    return HttpResponse("This is the About page")
+    return render(request, 'askmate/about.html')
 
 
 def login(request):
