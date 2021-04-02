cf0336303d69408a7c6e29d43ec4ee1e8501e0c2
CocoBloom <virag.baranyai2@gmail.com>
2021-04-02 19:58:21 +0200

    add view methods

--- askmate/views.py
+++ askmate/views.py
@@ -3,5 +3,22 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def home(request):
     return HttpResponse("This is the Home page")
+
+
+def about(request):
+    return HttpResponse("This is the About page")
+
+
+def login(request):
+    return HttpResponse("This is the Login page")
+
+
+def logout(request):
+    return HttpResponse("This is the Logout page")
+
+
+def registration(request):
+    return HttpResponse("This is the Registration page")
