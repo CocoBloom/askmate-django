80bff24fcb12b927c6c84a9e47b27849c2246c45
CocoBloom <virag.baranyai2@gmail.com>
2021-04-02 19:58:10 +0200

    add urlpatterns into askmate/urls.py

--- askmate/urls.py
+++ askmate/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='Main page'),
+    path('about/', views.about, name='About page'),
+    path('login/', views.login, name='Login page'),
+    path('registration/', views.registration, name='Reg page'),
+    path('logout', views.logout, name='Logout page'),
 ]
