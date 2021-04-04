3dcdd129ccb1cdc063bd04d8841fddd520baca6d
CocoBloom <virag.baranyai2@gmail.com>
2021-04-04 11:56:10 +0200

    delete unnecessary routes

--- askmate/urls.py
+++ askmate/urls.py
@@ -4,7 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.home, name='Main page'),
     path('about/', views.about, name='About page'),
-    path('login/', views.login, name='Login page'),
-    path('registration/', views.registration, name='Reg page'),
-    path('logout', views.logout, name='Logout page'),
 ]
